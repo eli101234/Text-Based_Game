8bc344cf3f41fb2a3a1acfa60711003296b93bbf
Eli Hernandez <elih717@gmail.com>
2021-04-16 19:21:29 -0400

    added to player docstring

--- player.py
+++ player.py
@@ -2,13 +2,17 @@
 """
 
 class Player:
-    """
+    """ Main player class. Holds the player's items and statuses
+    
+    Attributes:
+
     """
 
     def __init__():
+        """Creates new player object
         """
-        """
-    
+        
+        
     def attack():
         """
         """
