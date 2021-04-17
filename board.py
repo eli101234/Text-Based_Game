a47c1b35f04488b4873715bfd90799fc2cf7a444
Anternuy <anthonysadegray@gmail.com>
2021-04-16 22:56:07 -0400

    Update board.py

--- board.py
+++ board.py
@@ -3,10 +3,21 @@
 
 class Board:
     """
+    The environment in which the user plays the game.
+    The board will have 30 tiles to land on.
+    Monsters and Items will spawn randomly on each tile
+    
+    Attributes: 
+        filename(str): Path to a csv file that contains text descriptions 
+        for each tile.
     """
 
     def __init__():
         """
+        Initializes Board object
+        
+        Side Effects:
+           Tile objects are created
         """
     
     def board_state():
