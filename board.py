d34e2d1bc8033948358022cb543b62bc887cb7f4
Daniel Gurwah <dgurwah@umd.edu>
2021-04-28 23:32:39 -0400

    updated board.py
    put main and if __name__ == __main__ in here as well, added the print_board method as well

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 """
 """
+import random
 
 class Board:
     """
@@ -12,7 +13,7 @@ class Board:
         for each tile.
     """
 
-    def __init__():
+    def __init__(self):
         """
         Initializes Board object
         
@@ -20,14 +21,23 @@ class Board:
             Creates game environment
             30 Tile objects placed here
         """
+        self.place = 0
+        self.board_state = []
+        for i in range(1, 31):
+            self.board_state.append("_")
     
-    def board_state():
+    def print_board(self):
         """
         Tile objects are created
         
         Side Effects:
             Attributes: RNG spawns Monsters and Items
         """
+        for space in self.board_state:
+           print(space, end = " ") 
+
+    def change_board(self, index,  symbol = "x"):
+        self.board_state[index] = symbol
 
 def move():
     """
@@ -37,8 +47,10 @@ def move():
     Args:
         Dice(range/int):
     """
+    return random.randint(1, 6)
 
-def battle():
+
+def battle(player, monster):
     """
     Board function that engages user in battle
     
@@ -46,11 +58,53 @@ def battle():
         Calls Monster objects
         Calls Item objects
     """
+    
 
-def healing():
+
+def healing(item_name, player):
     """
     Function for player healing
     
     Args:
         Heal_item(int/float):
     """
+
+def main():
+
+    #create the board
+    new_game = Board()
+    
+    print("Hello and Welcome to the King of UMCP!")
+    name = input("What is your name challenger?")
+    print(f"Get ready {name}!")
+    
+    #create the player
+    #create the inventory
+
+    game_status = True
+
+    while (game_status):
+        option = input ("""
+        1. Roll the dice
+        2. Check inventory
+        3. Heal yourself
+        4. Flee
+        5. Check the board state
+        (ඞ is where you are, X is where you have been, and _ is undiscovered)
+        """)
+
+        if option == "1":
+            roll = move()
+            print(f"You rolled a {roll}!")
+            new_game.change_board(new_game.place, "X")
+            new_game.place += roll
+            new_game.change_board(new_game.place, "ඞ")
+        elif option == "5":
+            new_game.print_board()
+
+
+if __name__ == "__main__":
+    main()
+    
+
+    
