a4b9d75f449b0a80d0619a702b5b77261bc2e8fb
Eli Hernandez <elih717@gmail.com>
2021-04-30 14:08:46 -0400

    Added random monster encounter and added to flee option

--- board.py
+++ board.py
@@ -1,6 +1,7 @@
 """
 """
 import random
+import player
 
 class Board:
     """
@@ -58,6 +59,9 @@ def battle(player, monster):
         Calls Monster objects
         Calls Item objects
     """
+    while(player.hp > 0 or monster.hp > 0):
+        pass
+        
     
 
 
@@ -75,10 +79,15 @@ def main():
     new_game = Board()
     
     print("Hello and Welcome to the King of UMCP!")
+    #create the player
     name = input("What is your name challenger?")
+    hp = input("How much hp will you like?")
+    power = input("How strong are you?")
+    defense = input("How tanky are you?")
+    p1 = player.Player(name, hp, power, defense)
     print(f"Get ready {name}!")
     
-    #create the player
+    
     #create the inventory
 
     game_status = True
@@ -92,15 +101,26 @@ def main():
         5. Check the board state
         (ඞ is where you are, X is where you have been, and _ is undiscovered)
         """)
-
+        #maybe have a monster checker after each roll?
         if option == "1":
             roll = move()
             print(f"You rolled a {roll}!")
             new_game.change_board(new_game.place, "X")
             new_game.place += roll
             new_game.change_board(new_game.place, "ඞ")
+        elif option == "4":
+            #A way to quit this game
+            print(f"You decide this is all too much for you and flee, ending this journey...")
+            exit()
         elif option == "5":
+            #Checks the board
             new_game.print_board()
+            
+        #monster encounter!    
+        if(random.randint(1, 100) < 30):
+            #encounter a monster (30% chance)!
+            print(f"You have encountered a monster! But this feature is still in developement!")
+            pass
 
 
 if __name__ == "__main__":
