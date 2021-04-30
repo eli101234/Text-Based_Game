2a9f363e0ee5ea9bed0924f8aee680e7eb996643
Jelan Samatar <jelan.samatar@gmail.com>
2021-04-30 18:14:32 -0400

    Completed init and get items method

--- inventory.py
+++ inventory.py
@@ -1,35 +1,33 @@
-"""
-"""
+class Item:
+    """ Attributes--> name, damage, coeff"""
+    def __init__(self,name,hp):
+        self.name = name
+        self.hp = hp
+
 
 class Inventory:
-    """An inventory of objects available to player"""
+    """An inventory of healing objects available to player"""
     
     """Attributes:
-        inventory (dict): A dictionary containing items
+        inventory (dict): A dictionary containing health objects and their associated hp
     """
+    
     def __init__(self):
-        """Initializes a new inventory object.
-    """
+        self.inventory = {}
+        with open('items.csv', 'r', encoding = 'utf-8') as f:
+            for line in f:
+                line = line.strip()
+                line = line.split(',')
+                name = line[0]
+                hp = float(line[1])
+                self.inventory[name] = (hp)
     
-    def get_inventory():
+    def get_item(self,name):
         """
         """
-    
-
-    def add_item():
-        """Adds an item object to inventory
-        Args: an item object
-        Returns:
-        """
-    
-    def remove_item():
-        """Removes an item object from inventory
-        Args: an item object
-        Returns
-        """
-    def combine_item(self,item1,item2):
-        """ Attemps to two selected items for a coefficient raise
-    
-        Args: item objects"""
-       
-    
+        if name in self.inventory:
+            hp = self.inventory[name]
+            item= Item(name,hp) #presumes there's an item class to for item objects
+            return item
+        else: ## raise error
+            raise KeyError("Not found")  
