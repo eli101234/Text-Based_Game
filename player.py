d8a172031d710190d75e7cb514ef7f08878ca7a4
Eli Hernandez <elih717@gmail.com>
2021-04-29 21:57:56 -0400

    Added to attack_monster method

--- player.py
+++ player.py
@@ -22,18 +22,40 @@ class Player:
             power(int/float):
             defense(int/float):
         """
+        #Should we randomize the hp/power/Def?
         self.name = name
         self.hp = hp
         self.power = power
         self.defense = defense
         self.inventory = inventory
     
-    def attack_monster(self, monster):
+    def attack_monster(self, monster, weapon):
         """Does an attack against a monster
 
         Args:
             monster(Monster object): the monster the player is facing currently
+            weapon (string): What kind of weapon is in use for this combat
         """
+        #are we making it so defense nulifies power?
+        #Weapons: Hands, knife, sword, Hatchet, Testudo relic?
+        #armor: Helmet, vest, testudo blessing
+        if weapon == "sword":
+            monster.hp = monster.hp - (self.power*2 - self.power*(monster.defense/100))
+            return print(f'You unseathed the sword and strike the creature, dealing {(self.power - self.power*(monster.defense/100))} damage to them!')
+        elif weapon == "hatchet":
+            monster.hp = monster.hp - (self.power*1.5 - self.power*(monster.defense/100))
+            return print(f'You take out a hatchet and "axe" them a question, dealing {(self.power - self.power*(monster.defense/100))} damage to them!')
+        elif weapon == "knife":
+            monster.hp = monster.hp - (self.power*1.2 - self.power*(monster.defense/100))
+            return print(f"You take out the knife and slash the creature, dealing {(self.power - self.power*(monster.defense/100))} damage to them!")
+        #Bare hands
+        else:
+            monster.hp = monster.hp - (self.power - self.power*(monster.defense/100))
+            return print(f"You curl your hand into a fist and lung at the creature, dealing {(self.power - self.power*(monster.defense/100))} damage to them!")
+        
+        
+        
+        
 
 
         
