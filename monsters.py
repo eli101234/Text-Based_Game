e0c528780288821820ab4ea613e973977ff67aa9
Anternuy <anthonysadegray@gmail.com>
2021-04-30 22:19:16 -0400

    monster.py update

--- monsters.py
+++ monsters.py
@@ -8,18 +8,22 @@ class Monsters:
         filename(str): Path to a csv file that contains monsters and their
         hp, type, power, defense
     """
-    def __init__(self, filename):
+    def __init__(self, filepath):
         """Initializes new Monster object.
         
         Side effects:
             sets attributes Mon_type, hp, power, defense
         """
-        with open(filename, "r", encoding="utf-8") as f:
+        with open(Monsters.csv, "r", encoding="utf-8") as f:
             for line in f:
                 lineSplit = line.split("	")
                 Mon_type, hp, power, defense = lineSplit[0],lineSplit[1],lineSplit[2],lineSplit[3]
 
     def attack_player(self, player):
         """
+        Method for monster (self) to attack player.
+        
+        
         """
+        
     
