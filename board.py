60b91e124c0b1204de235392f817baabdb265e89
Anternuy <anthonysadegray@gmail.com>
2021-05-07 09:41:38 -0400

    Pull Request

--- board.py
+++ board.py
@@ -159,8 +159,7 @@ def main():
 
     #create the board
     new_game = Board()
-    #creating the monsters
-    monster_game = monsters.Monsters()
+    monsters_game = monsters()
     
     print("Hello and Welcome to the King of UMCP!")
     #create the player
@@ -190,6 +189,7 @@ def main():
             new_game.change_board(new_game.place, "X")
             new_game.place += roll
             new_game.change_board(new_game.place, "ඞ")
+<<<<<<< Updated upstream
             
             #monster encounter
             if(random.randint(0, 100) < 101):
@@ -199,6 +199,9 @@ def main():
 
             
             
+=======
+            monsters_game.monster()
+>>>>>>> Stashed changes
         elif option == "2": # using health items
             item = input("Enter an item: ").lower()
             item = inventory.get_item(item)
