c2b5d48137ecd49f7484fda9eb9f192a51565125
Daniel Gurwah <dgurwah@umd.edu>
2021-05-19 14:03:06 -0400

    Update textbased_game_test.py

--- textbased_game_test.py
+++ textbased_game_test.py
@@ -1,13 +1,16 @@
 import pytest
 from math import isclose
 import board, inventory as inv, monsters, player
+import builtins
+from unittest import mock
 
 #battle test
 def test_battle():
     
-    game = board.Board()
-    monsters = monsters.Monsters()
+    test_monsters = monsters.Monsters()
     test_player = player.Player("Tester", "assassin")
 
-    assert board.battle(test_player, monsters, "Aardvark") == True
-    assert board.battle(test_player, monsters, "Aric Bills") == False
+    with mock.patch("builtins.input", side_effect=["1"]):
+        assert board.battle(test_player, test_monsters, "Aardvark") == True
+    with mock.patch("builtins.input", side_effect=["1"]):
+        assert board.battle(test_player, test_monsters, "Aric Bills") == False
