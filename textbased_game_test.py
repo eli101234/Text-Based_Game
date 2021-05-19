a1de76d317ec96780da4e5933be3f3ec44f9d76e
Eli Hernandez <elih717@gmail.com>
2021-05-19 18:00:50 -0400

    pytest

--- textbased_game_test.py
+++ textbased_game_test.py
@@ -1,14 +1,14 @@
 import pytest
 from math import isclose
-import board, item as inv, monsters, player
+import board, item , monsters, player
 import builtins
 from unittest import mock
 
 #battle test
 def test_battle():
-    
+    test_inventory = board.Inventory()
     test_monsters = monsters.Monsters()
-    test_player = player.Player("Tester", "assassin")
+    test_player = player.Player("Tester", "assassin",test_inventory)
 
     with mock.patch("builtins.input", side_effect=["1"]):
         assert board.battle(test_player, test_monsters, "Aardvark") == True
@@ -16,8 +16,8 @@ def test_battle():
         assert board.battle(test_player, test_monsters, "Aric Bills") == False
         
 def test_attack_monster(capsys):
-    
-    test_player = player.Player("Test", "BRUISER")
+    test_inventory = board.Inventory()
+    test_player = player.Player("Test", "BRUISER", test_inventory)
     monster = monsters.Monsters(1,1,1)
     hitLow = test_player.power - 5
     test_player.attack_monster(monster)
@@ -30,8 +30,8 @@ def test_attack_monster(capsys):
 def test_attack_player():
     test_inventory = board.Inventory()
     test_player = player.Player("Test", "WARRIOR", test_inventory)
-    monster = monsters.Monster(30,30,30)
+    monster = monsters.Monsters(30,30,30)
     begin_hp = test_player.hp
-    monster.attackplayer(test_player)
+    monster.attack_player(test_player)
     end_hp = test_player.hp
     assert end_hp < begin_hp
